84a8c92b1084b648b51231638b0921343409e49a
Ali RAIKI <aliraiki@gmail.com>
2020-06-01 10:05:47 +0200

    Test card pricing

--- src/test/CardTest.java
+++ src/test/CardTest.java
@@ -22,31 +22,55 @@ public class CardTest {
 	 */
 	@Test
 	public void testCardVisitorPattern() {
+		CardVisitor visitor = new ConcreteCardVisitor();
 		
-		//1st scenario : The time credit is not used entirely
+		//1st scenario : The time credit is not used entirely (with a mechanical bike).
 		Card vlibre = new Vlibre(20);
 		Card vmax = new Vmax(20);
-		CardVisitor visitor = new ConcreteCardVisitor();
-		double vlibreCost = vlibre.accept(visitor, 65, new ElectricalBike()); //Ride cost if the ride lasted 65 minutes with an electrical bike, with a Vlibre card.
-		double vmaxCost = vmax.accept(visitor, 65, new ElectricalBike()); //Ride cost if the ride lasted 65 minutes with an electrical bike, with a Vmax card.
+		double vlibreCost = vlibre.accept(visitor, 65, new MechanicalBike()); //Ride cost if the ride lasted 65 minutes with a mechanical bike, with a Vlibre card.
+		double vmaxCost = vmax.accept(visitor, 65, new MechanicalBike()); //Ride cost if the ride lasted 65 minutes with an mechanical bike, with a Vmax card.
 
-		assertEquals(1, vlibreCost,0);
+		assertEquals(0, vlibreCost,0);
 		assertEquals(0, vmaxCost,0);
 		assertEquals(15, vlibre.getTimeCredit(),0);
 		assertEquals(15, vmax.getTimeCredit(),0);
 		
-		//2st scenario : The time credit is used entirely
+		//2nd scenario : The time credit is not used entirely (with an electrical bike)
 		Card vlibre2 = new Vlibre(20);
 		Card vmax2 = new Vmax(20);
-		double vlibreCost2 = vlibre2.accept(visitor, 135, new MechanicalBike()); //Ride cost if the ride lasted 135 minutes with a mechanical bike, with a Vlibre card.
-		double vmaxCost2 = vmax2.accept(visitor, 135, new MechanicalBike()); //Ride cost if the ride lasted 135 minutes with a mechanical bike, with a Vmax card.
-		double expectedVlibreCost = (double) (135-20-60)/60;
-		double expectedVmaxCost = (double) (135-20-60)/60;
+		double vlibreCost2 = vlibre2.accept(visitor, 65, new ElectricalBike()); //Ride cost if the ride lasted 65 minutes with an electrical bike, with a Vlibre card.
+		double vmaxCost2 = vmax2.accept(visitor, 65, new ElectricalBike()); //Ride cost if the ride lasted 65 minutes with an electrical bike, with a Vmax card.
+
+		assertEquals(1, vlibreCost2,0);
+		assertEquals(0, vmaxCost2,0);
+		assertEquals(15, vlibre2.getTimeCredit(),0);
+		assertEquals(15, vmax2.getTimeCredit(),0);
 		
-		assertEquals(expectedVlibreCost, vlibreCost2,0);
-		assertEquals(expectedVmaxCost, vmaxCost2,0);
-		assertEquals(0, vlibre2.getTimeCredit(),0);
-		assertEquals(0, vmax2.getTimeCredit(),0);
+		//3rd scenario : The time credit is used entirely (with a mechanical bike).
+		Card vlibre3 = new Vlibre(20);
+		Card vmax3 = new Vmax(20);
+		double vlibreCost3 = vlibre3.accept(visitor, 135, new MechanicalBike()); //Ride cost if the ride lasted 135 minutes with a mechanical bike, with a Vlibre card.
+		double vmaxCost3 = vmax3.accept(visitor, 135, new MechanicalBike()); //Ride cost if the ride lasted 135 minutes with a mechanical bike, with a Vmax card.
+		double expectedVlibreCost3 = (double) (135-20-60)/60;
+		double expectedVmaxCost3 = (double) (135-20-60)/60;
+		
+		assertEquals(expectedVlibreCost3, vlibreCost3,0);
+		assertEquals(expectedVmaxCost3, vmaxCost3,0);
+		assertEquals(0, vlibre3.getTimeCredit(),0);
+		assertEquals(0, vmax3.getTimeCredit(),0);
+		
+		//4th scenario : The time credit is used entirely (with an electrical bike).
+		Card vlibre4 = new Vlibre(20);
+		Card vmax4 = new Vmax(20);
+		double vlibreCost4 = vlibre4.accept(visitor, 135, new ElectricalBike()); //Ride cost if the ride lasted 135 minutes with an electrical bike, with a Vlibre card.
+		double vmaxCost4 = vmax4.accept(visitor, 135, new ElectricalBike()); //Ride cost if the ride lasted 135 minutes with an electrical bike, with a Vmax card.
+		double expectedVlibreCost4 = (double) (135-20-60)/30 + 1;
+		double expectedVmaxCost4 = (double) (135-20-60)/60;
+
+		assertEquals(expectedVlibreCost4, vlibreCost4,0);
+		assertEquals(expectedVmaxCost4, vmaxCost4,0);
+		assertEquals(0, vlibre4.getTimeCredit(),0);
+		assertEquals(0, vmax4.getTimeCredit(),0);
 	}
 
 }
