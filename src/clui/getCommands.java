ac53215d3b5aed0d9e13fb7c7cf20ae2e77cb42b
Ali RAIKI <aliraiki@gmail.com>
2020-06-02 11:37:22 +0200

    finish tests from file

--- src/clui/getCommands.java
+++ src/clui/getCommands.java
@@ -3,20 +3,21 @@ package clui;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class getCommands {
 	
 	public static void readTextFileLineByLine(String fileName) {
 		FileReader file = null;
 		BufferedReader reader = null;
+		ArrayList<String> lines = new ArrayList<String>();
 		
 		try {
 			file = new FileReader(fileName); 
 			reader = new BufferedReader(file); 
 			String line = "";
 			while ((line = reader.readLine()) != null) {
-				System.out.println(line);
-				ProcessCommandLine.executeCommand(line);
+				lines.add(line);
 			}
 		} catch (Exception e) {
 			System.err.println(e.getMessage());
@@ -34,5 +35,6 @@ public class getCommands {
 				}
 			}
 		}
+		ProcessCommandLine.executeCommand(lines);;
 	}
 }
